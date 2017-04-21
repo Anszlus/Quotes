4f0699a3a22e52849d1a4a5f5d08afd2ecde1052
Anszlus <anszlus12@gmail.com>
2017-04-21 17:04:56 +0200

    upload files

---
+++ js/quotes.js
@@ -0,0 +1,46 @@
+var quote = [
+  [
+    "Aby zer­wać z na­wykiem, wyrób so­bie in­ny, który go wymaże",
+    "Mark Twain"
+  ],
+  [
+    "Twój czas jest ograniczony, więc nie marnuj go na byciem kimś, kim nie jesteś",
+    "Steve Jobs"
+  ],
+  [
+    "Najlepszą zemstą jest ogromny sukces",
+    "Frank Sinatra"
+  ],
+  [
+    "Pudłujesz 100% strzałów, jeśli w ogóle ich nie wykonujesz",
+    "Wayne Gretzky"
+  ],
+  [
+    "Dwa najważniejsze dni Twojego życia to ten, w którym się urodziłeś oraz ten, w którym dowiedziałeś się, po co",
+    "Mark Twain"
+  ],
+  [
+    "Twoje życie staje się lepsze, tylko, gdy Ty stajesz się lepszym",
+    "Brian Tracy"
+  ],
+  [
+    "Na szczycie zawsze znajdzie się miejsce",
+    "Daniel Webster"
+  ],
+  [
+    "Człowiek, który goni dwa zające nie złapie ani jednego",
+    "Konfucjusz"
+  ],
+  [
+    "Za dwadzieścia lat bar­dziej będziesz żałował te­go, cze­go nie zro­biłeś, niż te­go, co zro­biłeś. Więc od­wiąż li­ny, opuść bez­pie­czną przys­tań. Złap w żag­le po­myślne wiat­ry. Podróżuj, śnij, od­kry­waj",
+    "Mark Twain"
+  ],
+  [
+    "Nic nie jest podawane na tacy – każdy zawsze trafia na jakieś przeszkody po drodze. Kiedy się pojawią, zastanów się jak je pokonać, a nie myśl o tym, że to już koniec drogi",
+    "Michael Jordan"
+  ],
+  [
+    "Nie ma nic złego w świętowaniu sukcesu, ale ważniejsze jest wyciągnięcie nauki z porażki",
+    "Bill Gates"
+  ]
+];
